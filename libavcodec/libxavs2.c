af2fa3b11394e262dd1e807a521cff254c6a283c
hwrenx <hwrenx@126.com>
2019-06-19 17:44:44 +0800

    configure lavc/libxavs2: fix for cavs2enc

--- libavcodec/libxavs2.c
+++ libavcodec/libxavs2.c
@@ -22,11 +22,12 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include "xavs2.h"
+#include "stdint.h"
+#include "cavs2enc.h"
 #include "mpeg12.h"
 #include "libavutil/avstring.h"
 
-#define xavs2_opt_set2(name, format, ...) do{ \
+#define cavs2enc_opt_set2(name, format, ...) do{ \
     char opt_str[16] = {0}; \
     int err; \
     av_strlcatf(opt_str, sizeof(opt_str), format, __VA_ARGS__); \
@@ -36,7 +37,7 @@
     }\
 } while(0);
 
-typedef struct XAVS2EContext {
+typedef struct CAVS2EncContext {
     AVClass *class;
 
     int lcu_row_threads;
@@ -49,24 +50,24 @@ typedef struct XAVS2EContext {
     int hierarchical_reference;
 
     void *encoder;
-    char *xavs2_opts;
+    char *cavs2enc_opts;
 
-    xavs2_outpacket_t packet;
-    xavs2_param_t *param;
+    cavs2_outpacket_t packet;
+    cavs2_param_t *param;
 
-    const xavs2_api_t *api;
+    const cavs2enc_api_t *api;
 
-} XAVS2EContext;
+} CAVS2EncContext;
 
-static av_cold int xavs2_init(AVCodecContext *avctx)
+static av_cold int cavs2enc_init(AVCodecContext *avctx)
 {
-    XAVS2EContext *cae= avctx->priv_data;
+    CAVS2EncContext *cae= avctx->priv_data;
     int bit_depth, code;
 
     bit_depth = avctx->pix_fmt == AV_PIX_FMT_YUV420P ? 8 : 10;
 
     /* get API handler */
-    cae->api = xavs2_api_get(bit_depth);
+    cae->api = cavs2enc_api_get(bit_depth);
     if (!cae->api) {
         av_log(avctx, AV_LOG_ERROR, "api get failed\n");
         return AVERROR_EXTERNAL;
@@ -78,28 +79,28 @@ static av_cold int xavs2_init(AVCodecContext *avctx)
         return AVERROR(ENOMEM);
     }
 
-    xavs2_opt_set2("Width",     "%d", avctx->width);
-    xavs2_opt_set2("Height",    "%d", avctx->height);
-    xavs2_opt_set2("BFrames",   "%d", avctx->max_b_frames);
-    xavs2_opt_set2("BitDepth",  "%d", bit_depth);
-    xavs2_opt_set2("Log",       "%d", cae->log_level);
-    xavs2_opt_set2("Preset",    "%d", cae->preset_level);
+    cavs2enc_opt_set2("Width",     "%d", avctx->width);
+    cavs2enc_opt_set2("Height",    "%d", avctx->height);
+    cavs2enc_opt_set2("BFrames",   "%d", avctx->max_b_frames);
+    cavs2enc_opt_set2("BitDepth",  "%d", bit_depth);
+    cavs2enc_opt_set2("Log",       "%d", cae->log_level);
+    cavs2enc_opt_set2("Preset",    "%d", cae->preset_level);
 
-    xavs2_opt_set2("IntraPeriodMax",    "%d", avctx->gop_size);
-    xavs2_opt_set2("IntraPeriodMin",    "%d", avctx->gop_size);
+    cavs2enc_opt_set2("IntraPeriodMax",    "%d", avctx->gop_size);
+    cavs2enc_opt_set2("IntraPeriodMin",    "%d", avctx->gop_size);
 
-    xavs2_opt_set2("ThreadFrames",      "%d", avctx->thread_count);
-    xavs2_opt_set2("ThreadRows",        "%d", cae->lcu_row_threads);
+    cavs2enc_opt_set2("ThreadFrames",      "%d", avctx->thread_count);
+    cavs2enc_opt_set2("ThreadRows",        "%d", cae->lcu_row_threads);
 
-    xavs2_opt_set2("OpenGOP",           "%d", !(avctx->flags & AV_CODEC_FLAG_CLOSED_GOP));
+    cavs2enc_opt_set2("OpenGOP",           "%d", !(avctx->flags & AV_CODEC_FLAG_CLOSED_GOP));
 
-    if (cae->xavs2_opts) {
+    if (cae->cavs2enc_opts) {
         AVDictionary *dict    = NULL;
         AVDictionaryEntry *en = NULL;
 
-        if (!av_dict_parse_string(&dict, cae->xavs2_opts, "=", ":", 0)) {
+        if (!av_dict_parse_string(&dict, cae->cavs2enc_opts, "=", ":", 0)) {
             while ((en = av_dict_get(dict, "", en, AV_DICT_IGNORE_SUFFIX))) {
-                xavs2_opt_set2(en->key, "%s", en->value);
+                cavs2enc_opt_set2(en->key, "%s", en->value);
             }
             av_dict_free(&dict);
         }
@@ -107,19 +108,19 @@ static av_cold int xavs2_init(AVCodecContext *avctx)
 
     /* Rate control */
     if (avctx->bit_rate > 0) {
-        xavs2_opt_set2("RateControl",   "%d", 1);
-        xavs2_opt_set2("TargetBitRate", "%"PRId64"", avctx->bit_rate);
-        xavs2_opt_set2("InitialQP",     "%d", cae->initial_qp);
-        xavs2_opt_set2("MaxQP",         "%d", cae->max_qp);
-        xavs2_opt_set2("MinQP",         "%d", cae->min_qp);
+        cavs2enc_opt_set2("RateControl",   "%d", 1);
+        cavs2enc_opt_set2("TargetBitRate", "%"PRId64"", avctx->bit_rate);
+        cavs2enc_opt_set2("InitialQP",     "%d", cae->initial_qp);
+        cavs2enc_opt_set2("MaxQP",         "%d", cae->max_qp);
+        cavs2enc_opt_set2("MinQP",         "%d", cae->min_qp);
     } else {
-        xavs2_opt_set2("InitialQP",     "%d", cae->qp);
+        cavs2enc_opt_set2("InitialQP",     "%d", cae->qp);
     }
 
 
     ff_mpeg12_find_best_frame_rate(avctx->framerate, &code, NULL, NULL, 0);
 
-    xavs2_opt_set2("FrameRate",   "%d", code);
+    cavs2enc_opt_set2("FrameRate",   "%d", code);
 
     cae->encoder = cae->api->encoder_create(cae->param);
 
@@ -131,7 +132,7 @@ static av_cold int xavs2_init(AVCodecContext *avctx)
     return 0;
 }
 
-static void xavs2_copy_frame_with_shift(xavs2_picture_t *pic, const AVFrame *frame, const int shift_in)
+static void cavs2enc_copy_frame_with_shift(cavs2_picture_t *pic, const AVFrame *frame, const int shift_in)
 {
     int j, k;
     for (k = 0; k < 3; k++) {
@@ -148,7 +149,7 @@ static void xavs2_copy_frame_with_shift(xavs2_picture_t *pic, const AVFrame *fra
     }
 }
 
-static void xavs2_copy_frame(xavs2_picture_t *pic, const AVFrame *frame)
+static void cavs2enc_copy_frame(cavs2_picture_t *pic, const AVFrame *frame)
 {
     int j, k;
     for (k = 0; k < 3; k++) {
@@ -160,11 +161,11 @@ static void xavs2_copy_frame(xavs2_picture_t *pic, const AVFrame *frame)
     }
 }
 
-static int xavs2_encode_frame(AVCodecContext *avctx, AVPacket *pkt,
+static int cavs2enc_encode_frame(AVCodecContext *avctx, AVPacket *pkt,
                       const AVFrame *frame, int *got_packet)
 {
-    XAVS2EContext *cae = avctx->priv_data;
-    xavs2_picture_t pic;
+    CAVS2EncContext *cae = avctx->priv_data;
+    cavs2_picture_t pic;
     int ret;
 
     /* create the XAVS2 video encoder */
@@ -177,15 +178,15 @@ static int xavs2_encode_frame(AVCodecContext *avctx, AVPacket *pkt,
         switch (frame->format) {
             case AV_PIX_FMT_YUV420P:
                 if (pic.img.in_sample_size == pic.img.enc_sample_size) {
-                    xavs2_copy_frame(&pic, frame);
+                    cavs2enc_copy_frame(&pic, frame);
                 } else {
                     const int shift_in = atoi(cae->api->opt_get(cae->param, "SampleShift"));
-                    xavs2_copy_frame_with_shift(&pic, frame, shift_in);
+                    cavs2enc_copy_frame_with_shift(&pic, frame, shift_in);
                 }
             break;
             case AV_PIX_FMT_YUV420P10:
                 if (pic.img.in_sample_size == pic.img.enc_sample_size) {
-                    xavs2_copy_frame(&pic, frame);
+                    cavs2enc_copy_frame(&pic, frame);
                     break;
                 }
             default:
@@ -196,7 +197,7 @@ static int xavs2_encode_frame(AVCodecContext *avctx, AVPacket *pkt,
 
         pic.i_state = 0;
         pic.i_pts   = frame->pts;
-        pic.i_type  = XAVS2_TYPE_AUTO;
+        pic.i_type  = CAVS2_TYPE_AUTO;
 
         ret = cae->api->encoder_encode(cae->encoder, &pic, &cae->packet);
 
@@ -209,7 +210,7 @@ static int xavs2_encode_frame(AVCodecContext *avctx, AVPacket *pkt,
         cae->api->encoder_encode(cae->encoder, NULL, &cae->packet);
     }
 
-    if ((cae->packet.len) && (cae->packet.state != XAVS2_STATE_FLUSH_END)){
+    if ((cae->packet.len) && (cae->packet.state != CAVS2_STATE_FLUSH_END)){
 
         if (av_new_packet(pkt, cae->packet.len) < 0){
             av_log(avctx, AV_LOG_ERROR, "packet alloc failed\n");
@@ -233,9 +234,9 @@ static int xavs2_encode_frame(AVCodecContext *avctx, AVPacket *pkt,
     return 0;
 }
 
-static av_cold int xavs2_close(AVCodecContext *avctx)
+static av_cold int cavs2enc_close(AVCodecContext *avctx)
 {
-    XAVS2EContext *cae = avctx->priv_data;
+    CAVS2EncContext *cae = avctx->priv_data;
     /* destroy the encoder */
     if (cae->api) {
         cae->api->encoder_destroy(cae->encoder);
@@ -247,7 +248,7 @@ static av_cold int xavs2_close(AVCodecContext *avctx)
     return 0;
 }
 
-#define OFFSET(x) offsetof(XAVS2EContext, x)
+#define OFFSET(x) offsetof(CAVS2EncContext, x)
 #define VE AV_OPT_FLAG_VIDEO_PARAM | AV_OPT_FLAG_ENCODING_PARAM
 
 static const AVOption options[] = {
@@ -258,18 +259,18 @@ static const AVOption options[] = {
     { "min_qp"          ,   "min qp for rate control" ,                 OFFSET(min_qp)          , AV_OPT_TYPE_INT, {.i64 = 20 },  0,      63,  VE },
     { "speed_level"     ,   "Speed level, higher is better but slower", OFFSET(preset_level)    , AV_OPT_TYPE_INT, {.i64 =  0 },  0,       9,  VE },
     { "log_level"       ,   "log level: -1: none, 0: error, 1: warning, 2: info, 3: debug", OFFSET(log_level)    , AV_OPT_TYPE_INT, {.i64 =  0 },  -1,       3,  VE },
-    { "xavs2-params"    ,   "set the xavs2 configuration using a :-separated list of key=value parameters", OFFSET(xavs2_opts), AV_OPT_TYPE_STRING, { 0 }, 0, 0, VE },
+    { "xavs2-params"    ,   "set the xavs2 configuration using a :-separated list of key=value parameters", OFFSET(cavs2enc_opts), AV_OPT_TYPE_STRING, { 0 }, 0, 0, VE },
     { NULL },
 };
 
 static const AVClass libxavs2 = {
-    .class_name = "XAVS2EContext",
+    .class_name = "CAVS2EncContext",
     .item_name  = av_default_item_name,
     .option     = options,
     .version    = LIBAVUTIL_VERSION_INT,
 };
 
-static const AVCodecDefault xavs2_defaults[] = {
+static const AVCodecDefault cavs2enc_defaults[] = {
     { "b",                "0" },
     { "g",                "48" },
     { "bf",               "7" },
@@ -281,13 +282,13 @@ AVCodec ff_libxavs2_encoder = {
     .long_name      = NULL_IF_CONFIG_SMALL("libxavs2 AVS2-P2/IEEE1857.4"),
     .type           = AVMEDIA_TYPE_VIDEO,
     .id             = AV_CODEC_ID_AVS2,
-    .priv_data_size = sizeof(XAVS2EContext),
-    .init           = xavs2_init,
-    .encode2        = xavs2_encode_frame,
-    .close          = xavs2_close,
+    .priv_data_size = sizeof(CAVS2EncContext),
+    .init           = cavs2enc_init,
+    .encode2        = cavs2enc_encode_frame,
+    .close          = cavs2enc_close,
     .capabilities   = AV_CODEC_CAP_DELAY | AV_CODEC_CAP_AUTO_THREADS,
     .pix_fmts       = (const enum AVPixelFormat[]) { AV_PIX_FMT_YUV420P, AV_PIX_FMT_YUV420P10, AV_PIX_FMT_NONE },
     .priv_class     = &libxavs2,
-    .defaults       = xavs2_defaults,
+    .defaults       = cavs2enc_defaults,
     .wrapper_name   = "libxavs2",
 } ;
