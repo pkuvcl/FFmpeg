554b9981653c9b13b0c6f4fd57de3b61bdd5c5d3
hwrenx <hwrenx@126.com>
2019-07-10 18:20:10 +0800

    lavc/libdavs3.c: clean code

--- libavcodec/libdavs2.c
+++ libavcodec/libdavs2.c
@@ -44,8 +44,9 @@ static av_cold int davs2_init(AVCodecContext *avctx)
 
     /* init the decoder */
     cad->param.threads      = avctx->thread_count;
-    av_log(avctx, AV_LOG_DEBUG, "Using thread: %d\n", avctx->thread_count);
-    cad->param.info_level   = 0;
+    cad->param.info_level   = av_log_get_level() > AV_LOG_INFO
+                                                 ? DAVS2_LOG_DEBUG
+                                                 : DAVS2_LOG_WARNING;
     cad->param.disable_avx  = !(cpu_flags & AV_CPU_FLAG_AVX &&
                                 cpu_flags & AV_CPU_FLAG_AVX2);
     cad->decoder            = davs2_decoder_open(&cad->param);
@@ -91,23 +92,23 @@ static int davs2_dump_frames(AVCodecContext *avctx, davs2_picture_t *pic, int *g
     }
 
     switch (pic->type) {
-        case DAVS2_PIC_I:
-        case DAVS2_PIC_G:
-            frame->pict_type = AV_PICTURE_TYPE_I;
-            break;
-        case DAVS2_PIC_P:
-        case DAVS2_PIC_S:
-            frame->pict_type = AV_PICTURE_TYPE_P;
-            break;
-        case DAVS2_PIC_B:
-            frame->pict_type = AV_PICTURE_TYPE_B;
-            break;
-        case DAVS2_PIC_F:
-            frame->pict_type = AV_PICTURE_TYPE_S;
-            break;
-        default:
-            av_log(avctx, AV_LOG_ERROR, "Decoder error: unknown frame type\n");
-            return AVERROR_EXTERNAL;
+    case DAVS2_PIC_I:
+    case DAVS2_PIC_G:
+        frame->pict_type = AV_PICTURE_TYPE_I;
+        break;
+    case DAVS2_PIC_P:
+    case DAVS2_PIC_S:
+        frame->pict_type = AV_PICTURE_TYPE_P;
+        break;
+    case DAVS2_PIC_B:
+        frame->pict_type = AV_PICTURE_TYPE_B;
+        break;
+    case DAVS2_PIC_F:
+        frame->pict_type = AV_PICTURE_TYPE_S;
+        break;
+    default:
+        av_log(avctx, AV_LOG_ERROR, "Decoder error: unknown frame type\n");
+        return AVERROR_EXTERNAL;
     }
 
     frame->width     = cad->headerset.width;
